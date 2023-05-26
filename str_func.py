de32ed9a957d86dfd609846c6a3cc4b8e3884eff
Vladimir Semikov <voesem@ya.ru>
2023-05-26 21:21:48 +0300

    docstring to capitalize_string and capitalize_every_word_in_string added

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def capitalize_string(text):
+    """Функция, которая делает все буквы заглавными"""
     return str.upper(text)
+
+
+def capitalize_every_word_in_string(text):
+    """Функция, которая делает заглавными первые буквы каждого слова в строке"""
+    return str.title(text)
